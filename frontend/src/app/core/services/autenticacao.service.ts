a75a0a8c28e4a1ce61b02d1eb7bc04845c434c53
Wendel Cutrim <wendelcutrim@hotmail.com>
2023-10-21 22:03:47 -0300

    feat: Criado método para realizar a autenticação do usuário na AutenticacaoService

--- frontend/src/app/core/services/autenticacao.service.ts
+++ frontend/src/app/core/services/autenticacao.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Login, Token } from '../types/type';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { UserService } from './user.service';
 
 @Injectable({
     providedIn: 'root',
@@ -10,9 +11,14 @@ import { Observable } from 'rxjs';
 export class AutenticacaoService {
     private apiUrl = environment.apiUrl;
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private userService: UserService) {}
 
-    autenticar(props: Login): Observable<Token> {
-        return this.http.post<Token>(`${this.apiUrl}/auth/login`, props);
+    autenticar(props: Login): Observable<HttpResponse<Token>> {
+        return this.http.post<Token>(`${this.apiUrl}/auth/login`, props, { observe: 'response' }).pipe(
+            tap((res) => {
+                const token = res.body?.access_token || '';
+                this.userService.saveToken(token);
+            }),
+        );
     }
 }
--- frontend/src/app/pages/cadastro/cadastro.component.ts
+++ frontend/src/app/pages/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { CadastroService } from 'src/app/core/services/cadastro.service';
 import { FormService } from 'src/app/core/services/form.service';
 import { PessoaUsuaria } from 'src/app/core/types/type';
@@ -12,6 +13,7 @@ export class CadastroComponent {
     perfilComponent: boolean = false;
     private formService = inject(FormService);
     private cadastroService = inject(CadastroService);
+    private router = inject(Router);
 
     onSubmit() {
         const form = this.formService.getCadastro();
@@ -22,6 +24,7 @@ export class CadastroComponent {
             this.cadastroService.cadastrar(novoCadstro).subscribe({
                 next: (res) => {
                     console.log('cadastro realizado com sucesso', res);
+                    this.router.navigate(['/login']);
                 },
                 error: (err) => {
                     console.error('[ERROR]: CadastroComponent.onSubmit', err);
