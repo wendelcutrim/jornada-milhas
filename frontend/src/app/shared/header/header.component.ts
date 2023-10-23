49c097396733c66b214f83c9a5890616febdde8a
Wendel Cutrim <wendelcutrim@hotmail.com>
2023-10-22 22:28:21 -0300

    feat: Adicionado rota de perfil no header quando o usuário estiver logado.

--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
     selector: 'app-header',
@@ -8,7 +9,16 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent {
     private router = inject(Router);
+    private userService = inject(UserService);
+
+    user$ = this.userService.returnUser();
+
     redirectHome() {
+        this.router.navigate(['/']);
+    }
+
+    logout() {
+        this.userService.logout();
         this.router.navigate(['/']);
     }
 }
